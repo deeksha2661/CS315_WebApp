8270055d374ba01f1c7bc1aecf23ea417e0255f5
deeksha <deekshav@iitk.ac.in>
2022-04-03 13:26:50 +0530

    added basic layout

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override'); 
 const Restaurant = require('./models/restaurant');
 
 mongoose.connect('mongodb://localhost:27017/dineout', {
@@ -18,16 +19,50 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
 
+app.use(express.urlencoded( {extended: true}));
+app.use(methodOverride('_method'));
+
 app.get('/', (req, res) => {
     res.render('home')
-})
+});
  
 app.get('/restaurants', async (req, res) => {
     const restaurants = await Restaurant.find({});
-    //console.log(restaurants);
     res.render('restaurants/index', { restaurants });
+});
+
+app.get('/restaurants/new', (req, res) => {
+    res.render('restaurants/new');
+});
+
+app.post('/restaurants', async (req, res) => {
+    const restaurant = new Restaurant(req.body.restaurant);
+    await restaurant.save();
+    res.redirect(`restaurants/${restaurant._id}`);
+});
+
+
+app.get('/restaurants/:id', async(req, res) => {
+    const restaurant = await Restaurant.findById(req.params.id);
+    res.render('restaurants/show', { restaurant });
+});
+
+app.get('/restaurants/:id/edit', async(req, res) =>{
+    const restaurant = await Restaurant.findById(req.params.id);
+    res.render('restaurants/edit', { restaurant });
 })
 
+app.put('/restaurants/:id', async (req, res) => {
+    const { id } = req.params;
+    const restaurant = await Restaurant.findByIdAndUpdate(id, { ...req.body.restaurant});
+    res.redirect(`/restaurants/${restaurant._id}`);
+})
+
+app.delete('/restaurants/:id', async (req, res) => {
+    const { id } = req.params;
+    await Restaurant.findByIdAndDelete(id);
+    res.redirect('/restaurants');
+})
 
 app.listen(3000, ()=>  {
     console.log('Serving on port 3000')
