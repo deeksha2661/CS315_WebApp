e4c8e460996d732d55aa66860461f5e97ddc66b2
deeksha <deekshav@iitk.ac.in>
2022-04-04 14:51:01 +0530

    added basic style

--- seeds/index.js
+++ seeds/index.js
@@ -18,14 +18,14 @@ const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
 const seedDB = async () => {
     await Restaurant.deleteMany({});
     
-    for(let i = 0; i < 50; i++){
-        const randm = Math.floor(Math.random() * cities.length);
-        const r = new Restaurant({
-            location: `${cities[randm].city}, ${cities[randm].admin_name}`,
-            title: `${sample(places).Name}`
-        })
-        await r.save();
-    }
+    // for(let i = 0; i < 50; i++){
+    //     const randm = Math.floor(Math.random() * cities.length);
+    //     const r = new Restaurant({
+    //         location: `${cities[randm].city}, ${cities[randm].admin_name}`,
+    //         title: `${sample(places).Name}`
+    //     })
+    //     await r.save();
+    // }
 }
 
 seedDB().then(() => {
