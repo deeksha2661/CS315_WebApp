91525202af6b79a2d3530badd0aceb5aba2c2c99
deeksha <deekshav@iitk.ac.in>
2022-04-07 16:45:22 +0530

    added maps functionality

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,14 +1,64 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 const Schema = mongoose.Schema;
 
-const RestaurantScehma = new Schema({
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+});
+
+const opts = { toJSON: { virtuals:true} };
+
+const RestaurantSchema = new Schema({
     title: String,
-    price: String,
+    images: [ImageSchema],
+    geometry: {
+        type: {
+          type: String, 
+          enum: ['Point'],
+          required: true
+        },
+        coordinates: {
+          type: [Number],
+          required: true
+        }
+    },
+    price: Number,
     description: String,
     cuisines: String,
     location: String,
     startTime: Date,
-    endTime: Date 
+    endTime: Date,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    reviews: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Review'
+        }
+    ]
+}, opts);
+
+
+RestaurantSchema.virtual('properties.popUpMarkup').get(function () {
+    return `<strong><a href="/restaurants/${this._id}">${this.title}</a><strong>
+    <p>${this.description.substring(0, 20)}...</p>`;
 });
 
-module.exports = mongoose.model('Restaurant', RestaurantScehma);
+RestaurantSchema.post('findOneAndDelete', async function(doc) {
+    if(doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        })
+    }
+})
+
+module.exports = mongoose.model('Restaurant', RestaurantSchema);
--- seeds/index.js
+++ seeds/index.js
@@ -4,10 +4,12 @@ const places = require('./seedHelpers')
 const Restaurant = require('../models/restaurant');
 
 mongoose.connect('mongodb://localhost:27017/dineout', {
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useNewUrlParser: true
 });
 
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
     console.log("Database connected");
@@ -18,16 +20,34 @@ const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
 const seedDB = async () => {
     await Restaurant.deleteMany({});
     
-    // for(let i = 0; i < 50; i++){
-    //     const randm = Math.floor(Math.random() * cities.length);
-    //     const r = new Restaurant({
-    //         location: `${cities[randm].city}, ${cities[randm].admin_name}`,
-    //         title: `${sample(places).Name}`
-    //     })
-    //     await r.save();
-    // }
+    for(let i = 0; i < 200; i++){
+        const randm = Math.floor(Math.random() * cities.length);
+        const price = Math.floor(Math.random() * 1000) + 10
+        const r = new Restaurant({
+            author: '624c9050ea7b70ce9cf1628b',
+            location: `${cities[randm].city}, ${cities[randm].admin_name}`,
+            title: `${sample(places).Name}`,
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sit amet dapibus odio. Etiam eu libero vitae neque semper mattis. Vivamus tincidunt, leo ut vehicula.',
+            price,
+            geometry: {
+                type: "Point",
+                coordinates: [cities[randm].lng, cities[randm].lat]
+            },
+            images: [
+                {
+                    url: 'https://res.cloudinary.com/duw4qslmx/image/upload/v1649197832/cld-sample.jpg',
+                    filename: 'cld-sample.jpg'
+                },
+                {
+                    url: 'https://res.cloudinary.com/duw4qslmx/image/upload/v1649197808/sample.jpg',
+                    filename: 'sample.jpg'
+                }
+            ]
+        })
+        await r.save();
+    }
 }
 
 seedDB().then(() => {
     mongoose.connection.close()
-});
+})
